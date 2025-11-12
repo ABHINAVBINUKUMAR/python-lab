d6c56605ae79decdd71d4b7d43e32c15b0349fec
ABHINAV BINUKUMAR <abhinavabhi03739@gmail.com>
2025-11-12 15:19:46 +0530

    Add files via upload

---
+++ file line by line.py
@@ -0,0 +1,15 @@
+file = open("file2.txt", "w")
+
+file.write("hello world!\nwelcome to python programing\nThank you")
+
+file.close()
+
+list = []
+
+file = open("file2.txt", "r")
+
+list = [line.strip() for line in file]
+
+file.close()
+
+print(list)
